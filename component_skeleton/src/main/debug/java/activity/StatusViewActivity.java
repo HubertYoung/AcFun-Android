0e9128b4ef57779e46fe9b363d1f53d5a2f6888a
JIUU <yj1344393464@163.com>
2018-05-05 22:32:52 +0800

    component_skeleton 完成5

---
+++ component_skeleton/src/main/debug/java/activity/StatusViewActivity.java
@@ -0,0 +1,54 @@
+package activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.kento.component_skeleton.R;
+import com.kento.component_skeleton.skeleton.ViewReplacer;
+
+
+public class StatusViewActivity extends AppCompatActivity {
+    private ViewReplacer mViewReplacer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.activity_status_view);
+        mViewReplacer = new ViewReplacer(findViewById(R.id.tv_content));
+        findViewById(R.id.btn_loading).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewReplacer.replace(R.layout.layout_progress);
+            }
+        });
+
+        findViewById(R.id.btn_error).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewReplacer.replace(R.layout.layout_error);
+            }
+        });
+
+        findViewById(R.id.btn_empty).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewReplacer.replace(R.layout.layout_empty_view);
+            }
+        });
+
+        findViewById(R.id.btn_content).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewReplacer.restore();
+            }
+        });
+
+    }
+
+    public void gotoSet(View view) {
+        Intent intent = new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS);
+        startActivity(intent);
+    }
+}
