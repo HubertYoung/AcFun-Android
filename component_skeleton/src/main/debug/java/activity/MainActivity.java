a2f81eeec37d6e32e064478263803abc801eb0dc
JIUU <yj1344393464@163.com>
2018-05-05 22:33:20 +0800

    component_skeleton 完成7

---
+++ component_skeleton/src/main/debug/java/activity/MainActivity.java
@@ -0,0 +1,49 @@
+package activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.kento.component_skeleton.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.skeleton_activity_main);
+        findViewById( R.id.btn_list).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RecyclerViewActivity.start(MainActivity.this, RecyclerViewActivity.TYPE_LINEAR);
+            }
+        });
+        findViewById(R.id.btn_grid).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RecyclerViewActivity.start(MainActivity.this, RecyclerViewActivity.TYPE_GRID);
+            }
+        });
+        findViewById(R.id.btn_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ViewActivity.start(MainActivity.this, ViewActivity.TYPE_VIEW);
+            }
+        });
+        findViewById(R.id.btn_Imgloading).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ViewActivity.start(MainActivity.this, ViewActivity.TYPE_IMG_LOADING);
+            }
+        });
+
+        findViewById(R.id.btn_status).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, StatusViewActivity.class));
+            }
+        });
+    }
+}
