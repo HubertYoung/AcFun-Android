a108659d567a84fbbac133ee5934d6acdc68eded
JIUU <yj1344393464@163.com>
2018-05-08 14:09:28 +0800

    删除一个lib

--- component_basic/src/main/java/com/kento/component/basic/commonwidget/dialog/dialog/listener/OnBtnClickL.java
+++
@@ -1,5 +0,0 @@
-package com.kento.component.basic.commonwidget.dialog.dialog.listener;
-
-public interface OnBtnClickL {
-	void onBtnClick();
-}
--- component_basic/src/main/java/com/kento/component/basic/commonwidget/dialog/dialog/listener/OnOperItemClickL.java
+++
@@ -1,8 +0,0 @@
-package com.kento.component.basic.commonwidget.dialog.dialog.listener;
-
-import android.view.View;
-import android.widget.AdapterView;
-
-public interface OnOperItemClickL {
-		void onOperItemClick( AdapterView< ? > parent, View view, int position, long id );
-	}
