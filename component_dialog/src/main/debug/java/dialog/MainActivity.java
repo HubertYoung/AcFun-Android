8ed6882e170e047985df8744ca9341f9af606ee9
HubertYoung <yj1344393464@163.com>
2018-08-07 17:08:36 +0800

    增加dialog

---
+++ component_dialog/src/main/debug/java/dialog/MainActivity.java
@@ -0,0 +1,174 @@
+package dialog;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.hubertyoung.component_dialog.R;
+import com.hubertyoung.dialog.BaseNiceDialog;
+import com.hubertyoung.dialog.NiceDialog;
+import com.hubertyoung.dialog.ViewConvertListener;
+import com.hubertyoung.dialog.ViewHolder;
+
+public class MainActivity extends AppCompatActivity{
+
+	@Override
+	protected void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.dialog_activity_main );
+	}
+
+
+	public void showDialog0(View view) {
+		NiceDialog.init()
+				.setLayoutId(R.layout.share_layout)
+				.setConvertListener(new ViewConvertListener() {
+					@Override
+					public void convertView( ViewHolder holder, final BaseNiceDialog dialog) {
+						holder.setOnClickListener(R.id.wechat, new View.OnClickListener() {
+							@Override
+							public void onClick(View v) {
+								Toast.makeText(MainActivity.this, "分享成功", Toast.LENGTH_SHORT).show();
+							}
+						});
+					}
+				})
+				.setDimAmount(0.3f)
+				.setShowBottom(true)
+				.show(getSupportFragmentManager());
+	}
+
+	public void showDialog1(View view) {
+		NiceDialog.init()
+				.setLayoutId(R.layout.friend_set_layout)
+				.setConvertListener(new ViewConvertListener() {
+					@Override
+					public void convertView(ViewHolder holder, final BaseNiceDialog dialog) {
+
+					}
+				})
+				.setShowBottom(true)
+				.setHeight(310)
+				.show(getSupportFragmentManager());
+	}
+
+	public void showDialog2(View view) {
+		NiceDialog.init()
+				.setLayoutId(R.layout.commit_layout)
+				.setConvertListener(new ViewConvertListener() {
+					@Override
+					public void convertView(ViewHolder holder, final BaseNiceDialog dialog) {
+						final EditText editText = holder.getView(R.id.edit_input);
+						editText.post(new Runnable() {
+							@Override
+							public void run() {
+								InputMethodManager imm = (InputMethodManager) getSystemService( Context.INPUT_METHOD_SERVICE);
+								imm.showSoftInput(editText, 0);
+							}
+						});
+					}
+				})
+				.setShowBottom(true)
+				.show(getSupportFragmentManager());
+	}
+
+	public void showDialog3(View view) {
+		NiceDialog.init()
+				.setLayoutId(R.layout.ad_layout)
+				.setConvertListener(new ViewConvertListener() {
+					@Override
+					public void convertView(ViewHolder holder, final BaseNiceDialog dialog) {
+						holder.setOnClickListener(R.id.close, new View.OnClickListener() {
+							@Override
+							public void onClick(View v) {
+								dialog.dismiss();
+							}
+						});
+					}
+				})
+				.setWidth(210)
+				.setOutCancel(false)
+				.setAnimStyle(R.style.EnterExitAnimation)
+				.show(getSupportFragmentManager());
+	}
+
+
+	public void showDialog4(View view) {
+		NiceDialog.init()
+				.setLayoutId(R.layout.loading_layout)
+				.setWidth(100)
+				.setHeight(100)
+				.setDimAmount(0)
+				.show(getSupportFragmentManager());
+	}
+
+	public void showDialog5(View view) {
+		ConfirmDialog.newInstance("1")
+				.setMargin(60)
+				.setOutCancel(false)
+				.show(getSupportFragmentManager());
+	}
+
+	public void showDialog6(View view) {
+		ConfirmDialog.newInstance("2")
+				.setMargin(60)
+				.setOutCancel(false)
+				.show(getSupportFragmentManager());
+	}
+
+	public static class ConfirmDialog extends BaseNiceDialog {
+		private String type;
+
+		public static ConfirmDialog newInstance(String type) {
+			Bundle bundle = new Bundle();
+			bundle.putString("type", type);
+			ConfirmDialog dialog = new ConfirmDialog();
+			dialog.setArguments(bundle);
+			return dialog;
+		}
+
+		@Override
+		public void onCreate(@Nullable Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			Bundle bundle = getArguments();
+			if (bundle == null) {
+				return;
+			}
+			type = bundle.getString("type");
+		}
+
+		@Override
+		public int intLayoutId() {
+			return R.layout.confirm_layout;
+		}
+
+		@Override
+		public void convertView(ViewHolder holder, final BaseNiceDialog dialog) {
+			if ("1".equals(type)) {
+				holder.setText(R.id.title, "提示");
+				holder.setText(R.id.message, "您已支付成功！");
+			} else if ("2".equals(type)) {
+				holder.setText(R.id.title, "警告");
+				holder.setText(R.id.message, "您的账号已被冻结！");
+			}
+			holder.setOnClickListener(R.id.cancel, new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					dialog.dismiss();
+				}
+			});
+
+			holder.setOnClickListener(R.id.ok, new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					dialog.dismiss();
+				}
+			});
+		}
+	}
+}
---
+++ component_dialog/src/main/java/com/hubertyoung/dialog/NiceDialog.java
@@ -0,0 +1,61 @@
+package com.hubertyoung.dialog;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+
+public class NiceDialog extends BaseNiceDialog {
+    private ViewConvertListener convertListener;
+
+    public static NiceDialog init() {
+        return new NiceDialog();
+    }
+
+    @Override
+    public int intLayoutId() {
+        return layoutId;
+    }
+
+    @Override
+    public void convertView(ViewHolder holder, BaseNiceDialog dialog) {
+        if (convertListener != null) {
+            convertListener.convertView(holder, dialog);
+        }
+    }
+
+
+    public NiceDialog setLayoutId(@LayoutRes int layoutId) {
+        this.layoutId = layoutId;
+        return this;
+    }
+
+    public NiceDialog setConvertListener(ViewConvertListener convertListener) {
+        this.convertListener = convertListener;
+        return this;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            convertListener = savedInstanceState.getParcelable("listener");
+        }
+    }
+
+    /**
+     * 保存接口
+     *
+     * @param outState
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable("listener", convertListener);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        convertListener = null;
+    }
+}
