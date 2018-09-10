570fc95259f05ca25cf9f2f2492f0f1031de4490
HubertYoung <yj1344393464@163.com>
2018-09-10 13:45:33 +0800

    增加支付组件项目

---
+++ component_pay/src/main/debug/java/pay/MainActivity.java
@@ -0,0 +1,16 @@
+package pay;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hubertyoung.component_pay.R;
+
+public class MainActivity extends AppCompatActivity{
+
+
+	@Override
+	protected void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.pay_activity_main );
+	}
+}
