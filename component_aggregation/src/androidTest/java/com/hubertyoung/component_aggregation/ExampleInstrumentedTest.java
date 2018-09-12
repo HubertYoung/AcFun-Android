1d4fd4a1d3c295564d3ce3ced3edb2f80c46a350
JIUU <yj1344393464@163.com>
2018-09-12 19:17:41 +0800

    修改组件名称

---
+++ component_aggregation/src/androidTest/java/com/hubertyoung/component_aggregation/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.hubertyoung.component_aggregation;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <drawable href="http://d.android.com/tools/testing">Testing documentation</drawable>
+ */
+@RunWith( AndroidJUnit4.class )
+public class ExampleInstrumentedTest {
+	@Test
+	public void useAppContext() {
+		// Context of the app under test.
+		Context appContext = InstrumentationRegistry.getTargetContext();
+
+		assertEquals( "com.hubertyoung.component_banner", appContext.getPackageName() );
+	}
+}
---
+++ component_aggregation/src/main/debug/java/aggregation/MyApplication.java
@@ -0,0 +1,27 @@
+package aggregation;
+
+import com.hubertyoung.common.CommonApplication;
+import com.hubertyoung.qqplatforms.platforms.qq.QQPlatFormConfig;
+import com.hubertyoung.wechatplatforms.platforms.weixin.WechatPlatFormConfig;
+import com.hubertyoung.weiboplatforms.platforms.weibo.WeiboPlatFormConfig;
+
+/**
+ * <br>
+ * function:
+ * <p>
+ *
+ * @author:HubertYoung
+ * @date:2018/9/10 20:09
+ * @since:V$VERSION
+ * @desc:pay
+ */
+public class MyApplication extends CommonApplication {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		QQPlatFormConfig.registerShare( "1106891112","QiIFegOZGaRmeC4S" );
+		WechatPlatFormConfig.registerShare( "wxf143a66eb7528d12","e408746be2a052ae1f294aa91595227c" );
+		WeiboPlatFormConfig.registerShare( "3136498027","","http://sns.whalecloud.com/sina2/callback" );
+	}
+}
