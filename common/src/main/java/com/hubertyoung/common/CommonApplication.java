b573f71074a03305b1a6040e25720a80225d92cd
HubertYoung <cnbjyangjian@gmail.com>
2018-10-19 15:15:25 +0800

    修改加载样式

--- common/src/main/java/com/hubertyoung/common/CommonApplication.java
+++ common/src/main/java/com/hubertyoung/common/CommonApplication.java
@@ -18,9 +18,12 @@ import com.hubertyoung.common.utils.ScreenHelper;
 import com.hubertyoung.common.utils.TimeUtil;
 import com.scwang.smartrefresh.header.MaterialHeader;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshFooterCreator;
 import com.scwang.smartrefresh.layout.api.DefaultRefreshHeaderCreator;
+import com.scwang.smartrefresh.layout.api.RefreshFooter;
 import com.scwang.smartrefresh.layout.api.RefreshHeader;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
 import com.tencent.mmkv.MMKV;
 
 import org.acra.ACRA;
@@ -64,7 +67,14 @@ public class CommonApplication extends Application {
 			public RefreshHeader createRefreshHeader( @NonNull Context context, @NonNull RefreshLayout layout ) {
 				layout.setPrimaryColorsId( R.color.colorPrimary, android.R.color.white );//全局设置主题颜色
 				layout.setHeaderHeight( 150 );
-				return new MaterialHeader( context );
+				return new MaterialHeader( context ).setColorSchemeResources( R.color.colorPrimary );
+			}
+		} );
+		SmartRefreshLayout.setDefaultRefreshFooterCreator( new DefaultRefreshFooterCreator() {
+			@NonNull
+			@Override
+			public RefreshFooter createRefreshFooter( @NonNull Context context, @NonNull RefreshLayout layout ) {
+				return new ClassicsFooter( context );
 			}
 		} );
 		ScreenHelper.initCustomDensity( this );
